91dc14ea5627e5e97bd7e52a97be7ff08d81d5dd
Pankaj Singh <p7ksingh@gmail.com>
2024-01-20 03:01:57 +0530

    persion class

---
+++ Person.java
@@ -0,0 +1,15 @@
+package com.p7ksingh.javaex;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private String name;
+    private Integer age;
+
+}
